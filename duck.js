630500123d451207c86462466b537d51d99c3877
winforfree <147972746+winforfree@users.noreply.github.com>
2024-01-23 08:03:29 +0600

    Create duck.js

---
+++ duck.js
@@ -0,0 +1,6 @@
+var gpl30 = "https://vobeluxo.com/cl/0dd58f11289c2de0?p1=&p2=&source=instagram&site=rxsalman";
+var bll30 = "https://vobeluxo.com/cl/e795686adf55c293?p1=&p2=&source=instagram&site=rxsalman";
+var robil30 = "https://vobeluxo.com/cl/a264878e7a3984c2?p1=&p2=&source=instagram&site=rxsalman";
+var airtel30 = "https://vobeluxo.com/cl/3ef91743f1e55c8c?p1=&p2=&source=instagram&site=rxsalman";
+var gpl300 = "https://vobeluxo.com/cl/0dd58f11289c2de0?p1=&p2=&source=instagram&site=rxsalman";
+var bll300 = "https://gozoboz.org/cl/8cd86b021893e56f?p1=&p2=&source=facebook&site=rxsalman";
